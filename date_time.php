1b01341c811b8f39a69713c5ab1f821191c761b3
silentpeople <chelsea_syuk90@yahoo.com>
2013-11-12 08:12:45 +0800

    Create date_time.php
    utk masa

---
+++ date_time.php
@@ -0,0 +1,12 @@
+<?php
+   //time zone 
+  date_default_timezone_set("Singapore");
+  //format for date
+  $date = date("D, d/m/Y ");
+  //format time
+  $time = date("H:i:s");
+  
+  echo $date;
+  echo $time;
+
+?>
